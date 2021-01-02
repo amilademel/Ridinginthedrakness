e5d1930e6b7300b45854c0863d2a30c35e8bf6ef
amilademel <amilademel176@gmail.com>
2021-01-02 14:48:08 +0530

    startRiding method add

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-document.getElementById('abc').innerHtml='Hellow';
+const con='chapter one paragraph one it all start when with the lonelyness..there was nobody on myside';
+function startRiding(){
+	var text=document.getElementById("abc").innerHTML;
+		if(text===con){
+			document.getElementById("abc").innerHTML = "Hell";
+		}else{
+			document.getElementById("abc").innerHTML = con;
+		}
+	
+}
